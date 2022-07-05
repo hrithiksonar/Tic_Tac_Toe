43ac39335f6785eb19b1947b850c6e98498df3de
Hrithik Kumar Sonar <107844855+hrithiksonar@users.noreply.github.com>
2022-07-05 18:10:59 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,77 @@
+package com.monocept.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	private ArrayList<Integer> abscissa = new ArrayList<Integer>();
+	private ArrayList<Integer> ordinate = new ArrayList<Integer>();
+	private String playerSign = null;
+	private static int SIZE = 3;
+	private static String gameBoard[][] = new String[SIZE][SIZE];
+
+	public TicTacToe(String move) {
+		this.playerSign = move;
+	}
+
+	public static String[][] getGameBoard() {
+		return gameBoard;
+	}
+
+	public ArrayList<Integer> getAbscissa() {
+		return abscissa;
+	}
+
+	public ArrayList<Integer> getOrdinate() {
+		return ordinate;
+	}
+
+	public static int getSIZE() {
+		return SIZE;
+	}
+
+	public String getMove() {
+		return playerSign;
+	}
+
+	public void insertMove(int xPosition, int yPosition) {
+		gameBoard[xPosition - 1][yPosition - 1] = this.playerSign;
+
+	}
+
+	public boolean winner() {
+		int diagonal1 = 0, diagonal2 = 0;
+		for (int i = 0; i < abscissa.size(); i++) {
+			if (Collections.frequency(abscissa, abscissa.get(i)) == SIZE) {
+				return true;
+			}
+			if (Collections.frequency(ordinate, ordinate.get(i)) == SIZE) {
+				return true;
+			}
+			if (abscissa.get(i) == ordinate.get(i)) {
+				diagonal1++;
+			}
+			if ((abscissa.get(i) + ordinate.get(i)) == SIZE + 1) {
+				diagonal2++;
+			}
+		}
+		if (diagonal1 == SIZE || diagonal2 == SIZE) {
+			return true;
+		}
+		return false;
+
+	}
+
+	public boolean checkPosition(int xPosition, int yPosition) {
+		if (gameBoard[xPosition - 1][yPosition - 1] != "X" && gameBoard[xPosition - 1][yPosition - 1] != "O") {
+			this.abscissa.add(xPosition);
+			this.ordinate.add(yPosition);
+			return true;
+		}
+		return false;
+
+	}
+
+}
---
+++ TicTacToeTest.java
@@ -0,0 +1,78 @@
+package com.monocept.test;
+
+import java.util.Scanner;
+import com.monocept.model.TicTacToe;
+
+public class TicTacToeTest {
+
+	public static void printLoop2(int i, String game[][]) {
+
+		for (int j = 0; j < TicTacToe.getSIZE(); j++) {
+			if (game[i][j] != null)
+				System.out.print("   " + game[i][j] + "	");
+			else
+				System.out.print("   	");
+			if (j == TicTacToe.getSIZE() - 1) {
+				continue;
+			}
+			System.out.print("|");
+		}
+
+	}
+
+	public static void printLoop1(String game[][]) {
+		for (int i = 0; i < TicTacToe.getSIZE(); i++) {
+			printLoop2(i, game);
+			if (i != TicTacToe.getSIZE() - 1)
+				System.out.println("\n--------------------------");
+		}
+		System.out.println("\n");
+	}
+
+	public static void play(TicTacToe player) {
+		Scanner scan = new Scanner(System.in);
+		System.out.printf("Player '%s', enter your move (row[1-3] column[1-3]): ", player.getMove());
+		int rowChoice = scan.nextInt();
+		int columnChoice = scan.nextInt();
+		if (player.checkPosition(rowChoice, columnChoice)) {
+			player.insertMove(rowChoice, columnChoice);
+		} else {
+			System.out.printf("This move at (%d,%d) is not valid. Try again...\n", rowChoice, columnChoice);
+			play(player);
+		}
+
+	}
+
+	public static void checkWin(TicTacToe player) {
+		if (player.winner()) {
+			System.out.printf("Player '%s' won!", player.getMove());
+			System.exit(0);
+		}
+	}
+
+	public static void main(String[] args) {
+		int count = 0;
+		TicTacToe playerX = new TicTacToe("X");
+		TicTacToe playerO = new TicTacToe("O");
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			play(playerX);
+			printLoop1(TicTacToe.getGameBoard());
+			checkWin(playerX);
+
+			count++;
+
+			if (count == 9) {
+				break;
+			}
+
+			play(playerO);
+			printLoop1(TicTacToe.getGameBoard());
+			checkWin(playerO);
+			count++;
+
+		}
+		System.out.println("IT'S A TIE");
+	}
+
+}
